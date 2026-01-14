7a5fee3be6b71cb14e642365e0313e6e8523825f
Hossam Elshal <hossam.elshaal@gmail.com>
2026-01-14 12:51:53 +0200

    D

--- app_one/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'App Test',
-    'author': 'Hossam Elshal',
-    'category': '',
-    'version': '17.0.0.1.0',
-    'depends': ['sale_management'],
-    'data': ['security/ir.model.access.csv','views/sale_order_view.xml'],
-    'application': True,
-}
