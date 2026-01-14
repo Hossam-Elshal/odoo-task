dae4e60edb47188f48b6ed784c3e21a472434ad9
Hossam Elshal <hossam.elshaal@gmail.com>
2026-01-14 12:54:35 +0200

    Add files via upload

---
+++ app_one/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'App Test',
+    'author': 'Hossam Elshal',
+    'category': '',
+    'version': '17.0.0.1.0',
+    'depends': ['base','sale_management'],
+    'data': ['security/security.xml',
+             'security/ir.model.access.csv',
+             'views/sale_order_view.xml'],
+    'application': True,
+}
